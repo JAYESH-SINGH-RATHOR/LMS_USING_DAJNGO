0de3563821d2baf587d0ecd272b3f2aabcba3115
JAYESH SINGH <jayeshsingh971@gmail.com>
2026-03-12 15:30:22 +0530

    NEW PROJECT

---
+++ lms/stundet/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StundetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stundet'
---
+++ lms/stundet/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request , "navBar.html")
+
+def about(request):
+    return render(request , "about.html")
+
+def contact(request):
+    return render(request , "contact.html")
+
+def register(request):
+    return render(request , "register.html")
+
